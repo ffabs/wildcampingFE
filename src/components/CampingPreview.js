b32b876349c94776e2c02adb1fa895544cc3507a
ffabs <fabioandferrari@gmail.com>
2021-01-22 18:31:40 +0100

    first data in the json file

--- src/components/CampingPreview.js
+++ src/components/CampingPreview.js
@@ -1,30 +1,44 @@
+import React, { Component } from 'react';
 import homeBackground from '../images/home-background.png';
 import Icon from './Icon';
 import '../App.css';
 import './CampingPreview.css';
 import {Link} from 'react-router-dom';
+import Data from '../camping-data.json';
 
-function CampingPreview() {
-  return (
-    <div className="camping-preview">
-        <img src={homeBackground} className="camping-preview-image" alt="wild-camping" />
-        <div className="camping-preview-text">
-            <div className="camping-preview-title">Grimbach - Black Forest</div>
-            <div className="camping-preview-priceandbooking">
-                <div className="camping-preview-price">€ 10</div>
-                <div className="camping-preview-booking">Booking required</div>
-            </div>
-            <div className="camping-preview-icons">
-                <Icon icon="fire" details="no"/>
-                <Icon icon="water" details="no"/>
-                <Icon icon="stars" details="no"/>
-            </div>
-            <Link to="/camping">
-              <div className="camping-preview-button">Details</div>
-            </Link>
-        </div>
-    </div>
-  );
+
+class CampingPreview extends Component {
+  
+  render() {
+
+    let name = Data[this.props.campingId]["name"]; 
+    let price = Data[this.props.campingId]["price"]; 
+    let bookingRequired = Data[this.props.campingId]["bookingRequired"];
+    let image = Data[this.props.campingId]["pictures"]["pic1"];
+
+    return (
+      <div className="camping-preview">
+          <img src={image} className="camping-preview-image" alt="wild-camping" />
+          <div className="camping-preview-text">
+              <div className="camping-preview-title">{name}</div>
+              <div className="camping-preview-priceandbooking">
+                  <div className="camping-preview-price">€ {price}</div>
+                  { bookingRequired === true &&
+                    <div className="camping-preview-booking">Booking required</div>
+                  }
+              </div>
+              <div className="camping-preview-icons">
+                  <Icon icon="fire" details="no"/>
+                  <Icon icon="water" details="no"/>
+                  <Icon icon="stars" details="no"/>
+              </div>
+              <Link to="/camping">
+                <div className="camping-preview-button">Details</div>
+              </Link>
+          </div>
+      </div>
+    );
+  }
 }
 
 export default CampingPreview;
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,23 +1,31 @@
+import React, { Component } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import CampingPreview from '../components/CampingPreview';
 import './Search.css';
+import Data from '../camping-data.json';
 
-function Search() {
-  return ( 
-    <div>
-        <Header /> 
-        <div className="search-list">
-            <CampingPreview />
-            <CampingPreview />
-            <CampingPreview />
-            <CampingPreview />
-            <CampingPreview />
-            <CampingPreview />
-        </div>
-        <Footer />
-    </div>  
-  );
+class Search extends Component {
+  
+  render() {
+
+    let campingId = Data["Camping in the Eifel"]["name"];
+
+    return ( 
+      <div>
+          <Header /> 
+          <div className="search-list">
+              <CampingPreview campingId={campingId} />
+              <CampingPreview campingId={campingId} />
+              <CampingPreview campingId={campingId} />
+              <CampingPreview campingId={campingId} />
+              <CampingPreview campingId={campingId} />
+              <CampingPreview campingId={campingId} />
+          </div>
+          <Footer />
+      </div>  
+    );
+  }
 }
 
 export default Search;
