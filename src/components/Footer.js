39780b75d4c1ec767d88befee41e55c59fcde6bb
ffabs <fabioandferrari@gmail.com>
2021-02-23 12:43:01 +0100

    adding socials to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,7 @@
 import '../App.css';
+import './Footer.css';
 import {Link} from 'react-router-dom';
+import { SocialIcon } from 'react-social-icons';
 
 function Footer() {
   return (
@@ -11,6 +13,28 @@ function Footer() {
             <div className="footer-element"> Searchpage</div>
         </Link>
         {/* <div className="footer-element"> Privacy policy </div> */}
+        <div className="footer has-text-centered">
+          <div>
+            Find us on
+          </div>
+          <div className="social-section">
+              <div className="social fac">
+                <SocialIcon className="icon" url="https://www.facebook.com/WildPeg-105294014944726" style={{ height: 50, width: 50 }} bgColor="#98A1AB" target="_blank" rel="noopener noreferrer"/>
+              </div>  
+              <div className="social">
+                <SocialIcon className="icon" url="https://mobile.twitter.com/WildPeg" style={{ height: 50, width: 50 }} bgColor="#98A1AB" target="_blank" rel="noopener noreferrer"/>
+              </div>
+              <div className="social insta">
+                <SocialIcon className="icon" url="https://www.instagram.com/noreply.wildpeg/" style={{ height: 50, width: 50 }} bgColor="#98A1AB" target="_blank" rel="noopener noreferrer"/>
+              </div>
+          </div>
+          <div>
+            All Rights Reserved
+          </div>
+          <div className="logo-name">
+            WildPeg
+          </div>
+        </div>
     </div>
   );
 }
